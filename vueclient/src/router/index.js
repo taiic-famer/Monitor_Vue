5eb78dc9c63d8b99228edd84b1d05c364b7c562a
zlw <2935718480@qq.com>
2021-01-12 20:09:48 +0800

    路由懒加载

--- vueclient/src/router/index.js
+++ vueclient/src/router/index.js
@@ -1,149 +1,169 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Main from '../views/Main.vue'
-import PlaneData from '../views/Monitor/PlaneData.vue'
-import PlanePlan from '../views/Monitor/PlanePlan.vue'
-import Gazebo from '../views/Monitor/Gazebo.vue'
-import Gazebo1 from '../views/Monitor/Gazebo1.vue'
-import MetricData from '../views/Monitor/MetricData.vue'
-import MetricCheck from '../views/Monitor/MetricCheck.vue'
-import Mqtt from '../views/Deployment/Mqtt.vue'
-import PlaneToMission from '../views/Deployment/PlaneToMission.vue'
-import MessageEdit from '../views/Management/MessageEdit.vue'
-import MessageList from '../views/Management/MessageList.vue'
-import PlaneEdit from '../views/Management/PlaneEdit.vue'
-import PlaneList from '../views/Management/PlaneList.vue'
-import MissionEdit from '../views/Management/MissionEdit.vue'
-import MissionList from '../views/Management/MissionList.vue'
-import ImageEdit from '../views/Management/DockerImageEdit.vue'
-import ImageList from '../views/Management/DockerImageList.vue'
-import Login from '../components/login.vue'
-import err404 from '../views/404.vue'
 
 Vue.use(VueRouter)
 
+const Main = () => import('../views/Main.vue')
+const PlaneData = () => import('../views/Monitor/PlaneData.vue')
+const PlanePlan = () => import('../views/Monitor/PlanePlan.vue')
+const Gazebo = () => import('../views/Monitor/Gazebo.vue')
+const Gazebo1 = () => import('../views/Monitor/Gazebo1.vue')
+const MetricData = () => import('../views/Monitor/MetricData.vue')
+const MetricCheck = () => import('../views/Monitor/MetricCheck.vue')
+const Mqtt = () => import('../views/Deployment/Mqtt.vue')
+const PlaneToMission = () => import('../views/Deployment/PlaneToMission.vue')
+const MessageEdit = () => import('../views/Management/MessageEdit.vue')
+const MessageList = () => import('../views/Management/MessageList.vue')
+const PlaneEdit = () => import('../views/Management/PlaneEdit.vue')
+const PlaneList = () => import('../views/Management/PlaneList.vue')
+const MissionEdit = () => import('../views/Management/MissionEdit.vue')
+const MissionList = () => import('../views/Management/MissionList.vue')
+const ImageEdit = () => import('../views/Management/DockerImageEdit.vue')
+const ImageList = () => import('../views/Management/DockerImageList.vue')
+const GzwebEdit = () => import('../views/Management/GzwebEdit.vue')
+const GzwebList = () => import('../views/Management/GzwebList.vue')
+import Login from '../components/login.vue'
+import err404 from '../views/404.vue'
+
+
+
 const routes = [
-	{
-		path: '/',
-		name: 'Login',
-		component: Login,
-		hidden: true
-	},
-	{
-		path: '/404',
-		name: '404',
-		component: err404,
-		hidden: true
-	},
-	{
-		path: '/main',
-		name: 'Main',
-		component: Main,
-		children: [
-			{
-				path: '/planes/data',
-				component: PlaneData
-			},
-			{
-				path: '/planes/plan',
-				component: PlanePlan
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+    hidden: true
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: err404,
+    hidden: true
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main,
+    children: [
+      {
+        path: '/planes/data',
+        component: PlaneData
       },
       {
-				path: '/gazebo',
-				component: Gazebo
+        path: '/planes/plan',
+        component: PlanePlan
       },
       {
-				path: '/gazebo1',
-				component: Gazebo1
-			},
-			{
-				path: '/metrics/data',
-				component: MetricData
-			},
-			{
-				path: '/metrics/check',
-				component: MetricCheck
-			},
-			{
-				path: '/mqtt',
-				component: Mqtt
-			},
+        path: '/gazebo',
+        component: Gazebo
+      },
       {
-				path: '/planeToMission',
-				component: PlaneToMission
-			},
-			{
-				path: '/messages/create',
-				component: MessageEdit
+        path: '/gazebo1',
+        component: Gazebo1
+      },
+      {
+        path: '/metrics/data',
+        component: MetricData
+      },
+      {
+        path: '/metrics/check',
+        component: MetricCheck
+      },
+      {
+        path: '/mqtt',
+        component: Mqtt
+      },
+      {
+        path: '/planeToMission',
+        component: PlaneToMission
+      },
+      {
+        path: '/messages/create',
+        component: MessageEdit
 
-			},
-			{
-				path: '/messages/edit/:id',
-				component: MessageEdit,
-				props: true
-			},
-			{
-				path: '/messages/list',
-				component: MessageList,
+      },
+      {
+        path: '/messages/edit/:id',
+        component: MessageEdit,
+        props: true
+      },
+      {
+        path: '/messages/list',
+        component: MessageList,
 
-			},
+      },
 
-			{
-				path: '/planes/create',
-				component: PlaneEdit
+      {
+        path: '/planes/create',
+        component: PlaneEdit
 
-			},
-			{
-				path: '/planes/edit/:id',
-				component: PlaneEdit,
-				props: true
-			},
-			{
-				path: '/planes/list',
-				component: PlaneList,
+      },
+      {
+        path: '/planes/edit/:id',
+        component: PlaneEdit,
+        props: true
+      },
+      {
+        path: '/planes/list',
+        component: PlaneList,
 
-			},
+      },
 
-			{
-				path: '/missions/create',
-				component: MissionEdit
+      {
+        path: '/missions/create',
+        component: MissionEdit
 
-			},
-			{
-				path: '/missions/edit/:id',
-				component: MissionEdit,
-				props: true
-			},
-			{
-				path: '/missions/list',
-				component: MissionList
-			},
+      },
+      {
+        path: '/missions/edit/:id',
+        component: MissionEdit,
+        props: true
+      },
+      {
+        path: '/missions/list',
+        component: MissionList
+      },
 
-			{
-				path: '/images/create',
-				component: ImageEdit
+      {
+        path: '/images/create',
+        component: ImageEdit
 
-			},
-			{
-				path: '/images/edit/:id',
-				component: ImageEdit,
-				props: true
-			},
-			{
-				path: '/images/list',
-				component: ImageList
-			},
-			
-		]
-	},
-	// 404 page must be placed at the end !!!
-	{ path: '*', redirect: '/404', hidden: true }
+      },
+      {
+        path: '/images/edit/:id',
+        component: ImageEdit,
+        props: true
+      },
+      {
+        path: '/images/list',
+        component: ImageList
+      },
+
+      {
+        path: '/gzweb/create',
+        component: GzwebEdit
+
+      },
+      {
+        path: '/gzweb/edit/:id',
+        component: GzwebEdit,
+        props: true
+      },
+      {
+        path: '/gzweb/list',
+        component: GzwebList
+      }
+
+    ]
+  },
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
 
 ]
 
 const router = new VueRouter({
-	routes,
-	mode: 'history'
+  routes,
+  mode: 'history'
 })
 
 export default router
